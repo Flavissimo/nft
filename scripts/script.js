47e7dbf198c1ff2546f8522fffd4798be8d37bfc
Flavissimo <Flavissimo@yandex.ru>
2022-05-30 17:47:16 +0300

    #4 Add button UP

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ const menuElement = document.querySelector(".header__menu");
 const menuList = document.querySelector(".header__list");
 
 for (let i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click",  () => {
+  coll[i].addEventListener("click", () => {
     coll[i].classList.toggle("active");
     const content = coll[i].nextElementSibling;
     if (content.style.maxHeight) {
@@ -18,3 +18,16 @@ menuElement.addEventListener("click", function () {
   menuElement.classList.toggle("toggle");
   menuList.classList.toggle("toggle");
 });
+
+window.addEventListener("scroll", function () {
+  const scroll = document.querySelector(".buttonTop");
+  scroll.classList.toggle("active", window.scrollY > 500);
+  /*console.log(scroll);*/
+});
+
+function scrollTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
